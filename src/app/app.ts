138a22b84104e6b5cb0af1edfc6bed5454d20d01
tgtdk <dhruvilharane188@gmail.com>
2025-12-15 23:58:14 +0530

    dx data grid addded

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxDataGridModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DxDataGridModule],
+
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'dxgrid-demo';
+  data: any[] = [];
+  selectedRowKeys: number[] = [];
+constructor() {
+    for (let i = 1; i <= 400; i++) {
+      this.data.push({
+        id: i,
+        name: 'Record ' + i
+      });
+    }
+  }
+
 }
